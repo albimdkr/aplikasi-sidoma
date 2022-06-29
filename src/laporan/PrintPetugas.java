ea6985ea26761c1fc95b4a120c1d0820647adf38
Albi Mudakar Nasyabi <albimdkr@gmail.com>
2022-06-29 11:07:51 +0700

    add report petugas

--- src/laporan/PrintPetugas.java
+++ src/laporan/PrintPetugas.java
@@ -440,14 +440,14 @@ public class PrintPetugas extends javax.swing.JFrame {
     }//GEN-LAST:event_BtnPrint21552011235MouseEntered
 
     private void BtnPrint21552011235MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnPrint21552011235MouseClicked
-//      try{
-//            String file = "/laporan/report_barista.jasper";
-//            JasperPrint print = JasperFillManager.fillReport(getClass().getResourceAsStream(file),null,koneksi.getKoneksi());
-//            JasperViewer.viewReport(print, false);
-//            
-//        }catch(JRException e){
-//            JOptionPane.showMessageDialog(rootPane, e);
-//        }
+      try{
+            String file = "/laporan/report_petugas.jasper";
+            JasperPrint print = JasperFillManager.fillReport(getClass().getResourceAsStream(file),null,koneksi.getKoneksi());
+            JasperViewer.viewReport(print, false);
+            
+        }catch(JRException e){
+            JOptionPane.showMessageDialog(rootPane, e);
+        }
     }//GEN-LAST:event_BtnPrint21552011235MouseClicked
 
     /**
